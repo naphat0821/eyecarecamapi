670a1ce7846a310a962fbea0f88a8465b5d66403
Naphat Phrompheng <naphat0821ph@gmail.com>
2025-09-04 19:10:15 +0700

    7h

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -56,7 +56,7 @@ export const login = async (req: Request, res: Response) => {
         if (!isMatch) return res.status(400).send('Password is incorrect');
 
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET || 'secret', {
-            expiresIn: '1h',
+            expiresIn: '7h',
         });
 
         res.status(200).json({ token });
